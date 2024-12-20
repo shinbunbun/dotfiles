a87dd9aa654a187653196c2afa03a36430185f23
Kousuke Takaki <98276492+yoseio@users.noreply.github.com>
2024-12-20 16:58:29 +0900

    initial commit

---
+++ cells/toplevel/darwinConfigurations.nix
@@ -0,0 +1,32 @@
+{
+  inputs,
+  cell,
+}: {
+  macOS = {
+    bee = {
+      system = "aarch64-darwin";
+      pkgs = inputs.nixpkgs;
+      home = inputs.home-manager;
+      darwin = inputs.nix-darwin;
+    };
+
+    imports = [
+      inputs.cells.core.darwinProfiles.default
+      inputs.cells.core.darwinProfiles.optimize
+    ];
+
+    home-manager.users.yoseio = {
+      imports = [
+        inputs.cells.core.homeProfiles.default
+      ];
+    };
+
+    users.users = {
+      yoseio = {
+        createHome = true;
+        home = "/Users/yoseio";
+        shell = inputs.nixpkgs.pkgs.zsh;
+      };
+    };
+  };
+}
