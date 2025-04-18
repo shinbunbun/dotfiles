008d66025b0febc93765c9c8cd17ade0222b2fcf
shinbunbun <34409044+shinbunbun@users.noreply.github.com>
2025-04-18 17:52:38 +0900

    debug ci

--- cells/repo/shells.nix
+++ cells/repo/shells.nix
@@ -6,15 +6,15 @@ let
   l = nixpkgs.lib // builtins;
   inherit (inputs) nixpkgs;
   inherit (inputs.std) std lib;
-  # darwinPackages =
-  #   if l.hasSuffix "darwin" nixpkgs.system then
-  #     [
-  #       {
-  #         package = inputs.nix-darwin.packages.${nixpkgs.system}.default;
-  #       }
-  #     ]
-  #   else
-  #     [ ];
+  darwinPackages =
+    if l.hasSuffix "darwin" nixpkgs.system then
+      [
+        {
+          package = inputs.nix-darwin.packages.${nixpkgs.system}.default;
+        }
+      ]
+    else
+      [ ];
 in
 l.mapAttrs (_: lib.dev.mkShell) {
   default = {
@@ -36,7 +36,7 @@ l.mapAttrs (_: lib.dev.mkShell) {
           category = "sops";
           package = ssh-to-age;
         }
-      ];
-      # ++ darwinPackages;
+      ]
+      ++ darwinPackages;
   };
 }
