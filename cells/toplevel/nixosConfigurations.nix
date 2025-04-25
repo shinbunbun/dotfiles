44842810151c5e530f9d2b10fb4c75832c50cb60
shinbunbun <34409044+shinbunbun@users.noreply.github.com>
2025-04-25 14:54:45 +0900

    debug nixos

--- cells/toplevel/nixosConfigurations.nix
+++ cells/toplevel/nixosConfigurations.nix
@@ -3,9 +3,13 @@
   cell,
 }:
 {
-  homeMachine = inputs.nixpkgs.legacyPackages."x86_64-linux".lib.nixosSystem {
-    system = "x86_64-linux";
-    modules = [
+  homeMachine = { config, pkgs, lib, ... }: {
+    bee = {
+      system = "x86_64-linux";
+      pkgs = inputs.nixpkgs;
+    };
+
+    imports = [
       ./hardwareConfigurations/homeMachine.nix
       inputs.cells.core.nixosProfiles.default
       inputs.cells.core.nixosProfiles.optimise
