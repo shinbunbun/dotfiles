98fe8470448c1f308da0920d48cf0f96abee8a50
shinbunbun <34409044+shinbunbun@users.noreply.github.com>
2025-04-26 12:07:15 +0900

    debug nixos

--- cells/toplevel/nixosConfigurations.nix
+++ cells/toplevel/nixosConfigurations.nix
@@ -26,4 +26,28 @@
         inputs.cells.core.nixosProfiles.optimise
       ];
     };
+  
+  default =
+    {
+      config,
+      pkgs,
+      lib,
+      ...
+    }:
+    {
+      bee = {
+        system = "x86_64-linux";
+        pkgs = inputs.nixpkgs;
+      };
+
+      meta = {
+        description = "Default NixOS configuration";
+      };
+
+      imports = [
+        ./hardwareConfigurations/homeMachine.nix
+        inputs.cells.core.nixosProfiles.default
+        inputs.cells.core.nixosProfiles.optimise
+      ];
+    };
 }
