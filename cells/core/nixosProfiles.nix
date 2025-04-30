062d2f57e5d045c82141461898ae7902d88d2ed0
shinbunbun <34409044+shinbunbun@users.noreply.github.com>
2025-04-30 16:51:01 +0900

    add nixos network config

--- cells/core/nixosProfiles.nix
+++ cells/core/nixosProfiles.nix
@@ -3,7 +3,10 @@
   cell,
 }:
 {
-  default = {
+  default = let
+    kubeMasterIP = "192.168.1.3";
+    kubeMasterHostname = "api.kube";
+  in  {
     # This value determines the NixOS release
     system.stateVersion = "21.11";
 
@@ -18,6 +21,34 @@
     # Use the systemd-boot EFI boot loader.
     boot.loader.systemd-boot.enable = true;
     boot.loader.efi.canTouchEfiVariables = true;
+
+    networking.hostName = "nixos";
+    networking.useDHCP = false;
+    networking.interfaces.eno1.useDHCP = true;
+    networking.interfaces.wlp1s0.useDHCP = false;
+    networking.enableIPv6 = true;
+
+    # Open ports in the firewall.
+    networking.firewall.allowedTCPPorts = [
+      6443
+      8888
+      2049
+    ];
+
+    networking.extraHosts = ''
+      ${kubeMasterIP} ${kubeMasterHostname}
+      192.168.1.4 nixos-desktop
+    '';
+
+    # Avahi config
+    services.avahi = {
+      enable = true;
+      publish = {
+        enable = true;
+        addresses = true;
+        workstation = true;
+      };
+    };
   };
 
   optimise = {
