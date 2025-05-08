3800f53bb0229d214fb36a8664d6b59e4cbcbea7
shinbunbun <34409044+shinbunbun@users.noreply.github.com>
2025-05-08 19:39:00 +0900

    debug ci

--- cells/core/nixosProfiles.nix
+++ cells/core/nixosProfiles.nix
@@ -1,30 +1,41 @@
 # cells/core/nixosProfiles.nix
 { inputs, cell }:
 {
-  default = { config, pkgs, lib, ... }: let
-    base = import ./base.nix { inherit config pkgs lib; };
-  in
-  base // lib.mkIf (builtins.getEnv "CI" == "") {
-    sops = {
-      defaultSopsFile = ../secrets/ssh-keys.yaml;
-      age.keyFile = "/var/lib/sops-nix/key.txt";
-      secrets."ssh_keys/bunbun" = {
-        owner = "bunbun";
+  default =
+    {
+      config,
+      pkgs,
+      lib,
+      ...
+    }:
+    let
+      base = import ./base.nix { inherit config pkgs lib; };
+    in
+    base
+  /*
+    // lib.mkIf (builtins.getEnv "CI" == "") {
+      sops = {
+        defaultSopsFile = ../secrets/ssh-keys.yaml;
+        age.keyFile = "/var/lib/sops-nix/key.txt";
+        secrets."ssh_keys/bunbun" = {
+          owner = "bunbun";
+        };
       };
-    };
-    users.users.bunbun.openssh.authorizedKeys.keyFiles = [
-      config.sops.secrets."ssh_keys/bunbun".path
-    ];
-  }
-  // lib.mkIf (builtins.getEnv "CI" != "") {
-    virtualisation.vmVariantWithBootLoader = {
-      virtualisation = {
-        memorySize = 2048;
-        cores = 2;
-        graphics = false;
+      users.users.bunbun.openssh.authorizedKeys.keyFiles = [
+        config.sops.secrets."ssh_keys/bunbun".path
+      ];
+    }
+    // lib.mkIf (builtins.getEnv "CI" != "") {
+      virtualisation.vmVariantWithBootLoader = {
+        virtualisation = {
+          memorySize = 2048;
+          cores = 2;
+          graphics = false;
+        };
       };
-    };
-  };
+    }
+  */
+  ;
   optimise = {
     nix.settings.auto-optimise-store = true;
     nix.gc = {
