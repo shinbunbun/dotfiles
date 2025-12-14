7436930a8a8e7bddbd110d7cd37d08ebce9c0a51
shinbunbun <34409044+shinbunbun@users.noreply.github.com>
2025-12-15 01:52:27 +0900

    fix: exclude unused network interfaces from NetworkInterfaceDown alert
    enp4s0とenp6s0f1は物理的に未接続のNICのため、アラート対象から除外

--- systems/nixos/modules/services/alertmanager.nix
+++ systems/nixos/modules/services/alertmanager.nix
@@ -327,7 +327,7 @@ in
             # ネットワークインターフェースダウン
             {
               alert = "NetworkInterfaceDown";
-              expr = "node_network_up{device!~\"lo|docker.*|veth.*|br.*|tap.*|tun.*|wlp[0-9]s0|wg.*\"} == 0";
+              expr = "node_network_up{device!~\"lo|docker.*|veth.*|br.*|tap.*|tun.*|wlp[0-9]s0|wg.*|enp4s0|enp6s0f1\"} == 0";
               for = "2m";
               labels = {
                 severity = "critical";
