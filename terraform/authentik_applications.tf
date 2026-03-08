ed6584dc2611737387d84edada3f06562c3240e8
shinbunbun <34409044+shinbunbun@users.noreply.github.com>
2026-03-08 11:50:05 +0900

    nixos-observabilityの参照を更新（インデックステンプレート登録削除）

--- terraform/authentik_applications.tf
+++ terraform/authentik_applications.tf
@@ -5,33 +5,33 @@
 */
 
 resource "authentik_application" "couchdb" {
-  name              = "CouchDB"
-  slug              = "couchdb"
-  protocol_provider = authentik_provider_oauth2.couchdb.id
+  name               = "CouchDB"
+  slug               = "couchdb"
+  protocol_provider  = authentik_provider_oauth2.couchdb.id
   policy_engine_mode = "any"
 }
 
 resource "authentik_application" "cloudflare_zero_trust" {
-  name              = "Cloudflare Zero Trust"
-  slug              = "cloudflare-zero-trust"
-  protocol_provider = authentik_provider_oauth2.cloudflare_zero_trust.id
-  meta_launch_url   = "https://shinbunbun.cloudflareaccess.com/cdn-cgi/access/login/cloudflare-zero-trust"
+  name               = "Cloudflare Zero Trust"
+  slug               = "cloudflare-zero-trust"
+  protocol_provider  = authentik_provider_oauth2.cloudflare_zero_trust.id
+  meta_launch_url    = "https://shinbunbun.cloudflareaccess.com/cdn-cgi/access/login/cloudflare-zero-trust"
   policy_engine_mode = "any"
 }
 
 resource "authentik_application" "grafana" {
-  name              = "Grafana"
-  slug              = "grafana"
-  protocol_provider = authentik_provider_oauth2.grafana.id
-  meta_launch_url   = "https://grafana.shinbunbun.com"
+  name               = "Grafana"
+  slug               = "grafana"
+  protocol_provider  = authentik_provider_oauth2.grafana.id
+  meta_launch_url    = "https://grafana.shinbunbun.com"
   policy_engine_mode = "any"
 }
 
 resource "authentik_application" "opensearch_dashboards" {
-  name              = "OpenSearch Dashboards"
-  slug              = "opensearch-dashboards"
-  protocol_provider = authentik_provider_oauth2.opensearch_dashboards.id
-  meta_launch_url   = "https://opensearch.shinbunbun.com"
+  name               = "OpenSearch Dashboards"
+  slug               = "opensearch-dashboards"
+  protocol_provider  = authentik_provider_oauth2.opensearch_dashboards.id
+  meta_launch_url    = "https://opensearch.shinbunbun.com"
   policy_engine_mode = "any"
 }
 
@@ -44,8 +44,8 @@ resource "authentik_application" "argocd" {
 }
 
 resource "authentik_application" "wg_lease" {
-  name              = "wg-lease"
-  slug              = "wg-lease"
-  protocol_provider = authentik_provider_proxy.wg_lease.id
+  name               = "wg-lease"
+  slug               = "wg-lease"
+  protocol_provider  = authentik_provider_proxy.wg_lease.id
   policy_engine_mode = "any"
 }
